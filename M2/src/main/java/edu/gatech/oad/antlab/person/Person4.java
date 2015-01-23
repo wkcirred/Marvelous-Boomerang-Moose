393519db2ee3910664a69b0e02e6d7c88afa7df5
Derrick Williams <derrickw@gatech.edu>
2015-01-23 15:36:05 -0500

    Cleaned up crap in Person4.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,24 +30,14 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-<<<<<<< Updated upstream
+
       String output = "";
       
       for (int i = 0; i < input.length(); i++) {
 	  output += (char)(input.charAt(i) + 1);
-	}
+      }
 
       return output;
-=======
-
-	String output = "";
-
-        for (int i = 0; i < input.length(); i++) {
-            output += (char)(input.charAt(i) + 1);
--	}    
-        
-        return null;
->>>>>>> Stashed changes
     }
     
     /**
