6ac188d925a51d9d81c807bab6762fade62ffcf4
Richard Valentine <rvalentine7@gatech.edu>
2015-02-03 15:41:57 -0500

    M4
    The finished version of M3.  Begin M4 work with this file.

---
+++ M4/ShoppingWithFriends/app/src/main/java/marvelousboomerangmoose/shoppingwithfriends/MainActivity.java
@@ -0,0 +1,81 @@
+package marvelousboomerangmoose.shoppingwithfriends;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+        /*if (LoginActivity.loggedIn == 1) {
+            startActivity(new Intent(this, HomeActivity.class));
+        }*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * When the login button is clicked, this transitions to the login screen.
+     *
+     * @param v the button being clicked
+     */
+    public void buttonOnClick(View v) {
+        Button button=(Button) v;
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
