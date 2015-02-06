82aeb9a2212cc443f5053948b91cb1c9197ffda9
Richard Valentine <rvalentine7@gatech.edu>
2015-02-05 20:01:30 -0500

    M4 Logout + Javadoc
    Added the logout button and did some javadoc comments.  Some comments
    still need to be made.

--- M4/ShoppingWithFriends/app/src/main/java/marvelousboomerangmoose/shoppingwithfriends/HomeActivity.java
+++ M4/ShoppingWithFriends/app/src/main/java/marvelousboomerangmoose/shoppingwithfriends/HomeActivity.java
@@ -1,5 +1,6 @@
 package marvelousboomerangmoose.shoppingwithfriends;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 
 
 public class HomeActivity extends ActionBarActivity {
@@ -31,6 +33,15 @@ public class HomeActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
+    }
+
+    /**
+     * Logs the user out of the program.
+     * @param v the button being clicked
+     */
+    public void logoutOnClick(View v) {
+        Button button=(Button) v;
+        startActivity(new Intent(this, MainActivity.class));
     }
 
     @Override
