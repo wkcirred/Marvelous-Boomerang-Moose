c3cb1ecb1b4e9656ad24321c0a5d3e3027ff029a
Derrick Williams <derrickw@gatech.edu>
2015-02-24 07:21:21 -0500

    M6 pretty much done; except Richard can add sales report data

--- M6/ShoppingWithFriends/app/src/main/java/marvelousboomerangmoose/shoppingwithfriends/CurrentFriendListActivity.java
+++ M6/ShoppingWithFriends/app/src/main/java/marvelousboomerangmoose/shoppingwithfriends/CurrentFriendListActivity.java
@@ -6,14 +6,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
-public class NewFriendListActivity extends ActionBarActivity {
+public class CurrentFriendListActivity extends ActionBarActivity {
     private Intent intent;
     private String message;
     private static ArrayList<String> arrayList;
@@ -29,9 +28,22 @@ public class NewFriendListActivity extends ActionBarActivity {
             HashMap<String, User> friends = MainActivity.loggedInUser.getFriendList();
             for (String key : friends.keySet()) {
                 User friend = friends.get(key);
-                listAdapter.add(friend.getFirst() + " " + friend.getLast() + " " + friend.getEmail());
+                listAdapter.add(friend.getFirst() + " " + friend.getLast());
+                //listAdapter.add(friend.getFirst() + " " + friend.getLast() + " " + friend.getEmail());
             }
             myListView.setAdapter(listAdapter);
+            myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    Intent intent = new Intent(CurrentFriendListActivity.this,FriendDetailActivity.class);
+                    intent.putExtra("User Name",friends.);
+                }
+            });
+
+
+
+
     }
 
 
@@ -57,13 +69,23 @@ public class NewFriendListActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
+
+    /**
+     * Changes to add friend activity screen
+     * @param v - button is clicked
+     */
     public void addFriendButtonOnClick(View v){
-        //Button button=(Button) v;
         startActivity(new Intent(this, AddFriendActivity.class));
     }
+
+    /**
+     * Changes back to home screen
+     * @param v - button is clicked
+     */
     public void homeButtonOnClick(View v){
         startActivity(new Intent(this, HomeActivity.class));
-
     }
 
 }
