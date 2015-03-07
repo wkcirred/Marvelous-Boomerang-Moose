88a93f706e42e80fff3cc86db8c3418c46c7670e
Derrick Williams <derrickw@gatech.edu>
2015-03-07 17:34:47 -0500

    Added M8 folder

---
+++ M8/ShoppingWithFriends/app/src/main/java/marvelousboomerangmoose/shoppingwithfriends/Sale.java
@@ -0,0 +1,31 @@
+package marvelousboomerangmoose.shoppingwithfriends;
+
+import java.util.ArrayList;
+
+/**
+ * Holds a list of the reported sales.
+ */
+public class Sale {
+    private ArrayList salesList;
+
+    // Constructor
+    public Sale(ArrayList salesList) {
+        this.salesList = salesList;
+    }
+
+    /**
+     * Gets the sales list.
+     * @return the list of sales
+     */
+    public ArrayList getSalesList() {
+        return salesList;
+    }
+
+    /**
+     * Sets the sales list.
+     * @param salesList the list of sales
+     */
+    public void setArrayList(ArrayList salesList) {
+        this.salesList = salesList;
+    }
+}
